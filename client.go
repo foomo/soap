b500ce32d2d9dd492f8d120915fa40a77aac80f8
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-03-17 17:34:56 +0100

    Fixed issue in Call(): Calls with nil as parameter for response (such as sendOrder() or sendTLog(), which expect an empty response and only look for the http header) paniced if the response was not empty (SOAP-Fault). Response parameters being nil are now replaced by a dummy struct and the SOAP-FAULT is returned via the error message.

--- client.go
+++ client.go
@@ -2,8 +2,11 @@ package soap
 
 import (
 	"bytes"
+	"encoding/json"
 	"encoding/xml"
+	"errors"
 	"io/ioutil"
+	"log"
 	"net"
 	"net/http"
 	"time"
@@ -67,6 +70,7 @@ func NewClient(url string, auth *BasicAuth, tr *http.Transport) *Client {
 
 // Call make a SOAP call
 func (s *Client) Call(soapAction string, request, response interface{}) (httpResponse *http.Response, err error) {
+
 	envelope := Envelope{}
 
 	envelope.Body.Content = request
@@ -97,11 +101,12 @@ func (s *Client) Call(soapAction string, request, response interface{}) (httpRes
 		tr = http.DefaultTransport.(*http.Transport)
 	}
 	client := &http.Client{Transport: tr}
-	l("POST to", s.url, "with", string(xmlBytes))
+	l("POST to", s.url, "with\n", string(xmlBytes))
 	httpResponse, err = client.Do(req)
 	if err != nil {
 		panic(err)
 	}
+
 	defer httpResponse.Body.Close()
 
 	rawbody, err := ioutil.ReadAll(httpResponse.Body)
@@ -115,15 +120,34 @@ func (s *Client) Call(soapAction string, request, response interface{}) (httpRes
 
 	l("response", string(rawbody))
 	respEnvelope := new(Envelope)
-	respEnvelope.Body = Body{Content: response}
+	type Dummy struct {
+	}
+	if response == nil {
+		// This may be the case, if an empty response is expected, but a SOAP Fault is received.
+		// In this case unmarshalling of the rawbody would fail without using the Dummy, because response being a nil-pointer.
+		respEnvelope.Body = Body{Content: &Dummy{}}
+	} else {
+		respEnvelope.Body = Body{Content: response}
+	}
+
 	err = xml.Unmarshal(rawbody, respEnvelope)
 	if err != nil {
+		log.Println("soap/client.go Call(): COULD NOT UNMARSHAL")
 		panic(err)
 	}
 
 	fault := respEnvelope.Body.Fault
+	// If a SOAP Fault is received, try to jsonMarshal it and return it via the error.
 	if fault != nil {
-		panic(fault)
+		log.Println("Received SOAP FAULT")
+		jsonBytes, err := json.MarshalIndent(respEnvelope.Body.Fault, "", "	")
+		if err != nil {
+			log.Println("soap/client.go Call(): could not jsonMarhal SOAP-Fault")
+			return httpResponse, err
+		}
+		log.Println(string(jsonBytes))
+		return httpResponse, errors.New("Received SOAP Fault:\n" + string(jsonBytes))
+
 	}
 	return
 }
